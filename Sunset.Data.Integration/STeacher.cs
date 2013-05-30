62c4843a9ca083a35c571715b32a4a623756fbbb
kunhsiang <kunhsiang.chang@ischool.com.tw>
2013-05-30 11:37:14 +0800

    清除沒用到的程式碼。

--- Sunset.Data.Integration/STeacher.cs
+++ Sunset.Data.Integration/STeacher.cs
@@ -119,23 +119,6 @@ namespace Sunset.Data.Integration
         }
 
         /// <summary>
-        /// 從資料列載入
-        /// </summary>
-        /// <param name="Row"></param>
-        /// <param name="DSNS"></param>
-        //public void Load(DataRow Row, string DSNS)
-        //{
-        //    string ID = Row.Field<string>("id");
-        //    string Name = Row.Field<string>("teacher_name");
-        //    string NickName = Row.Field<string>("nickname");
-        //    string FullName = Name + NickName;
-
-        //    this.ID = FullName;
-        //    this.Name = FullName;
-        //    this.SourceIDs.Add(new SourceID(DSNS,ID));
-        //}
-
-        /// <summary>
         /// 轉換成XElement
         /// </summary>
         /// <returns></returns>
@@ -158,34 +141,6 @@ namespace Sunset.Data.Integration
         #endregion
 
         #region static method
-        ///// <summary>
-        ///// 從單一資料來源取得教師資料
-        ///// </summary>
-        ///// <param name="Connection">連線物件</param>
-        ///// <param name="strSQL">SQL指令</param>
-        ///// <returns></returns>
-        //private static List<STeacher> Select(Connection Connection, string strSQL)
-        //{
-        //    #region 取得教師原始資料
-        //    QueryHelper Helper = new QueryHelper(Connection);
-        //    DataTable vDataTable = Helper.Select(strSQL);
-        //    List<STeacher> Teachers = new List<STeacher>();
-        //    #endregion
-
-        //    #region 將原始資料轉換成教師資料
-        //    foreach (DataRow Row in vDataTable.Rows)
-        //    {
-        //        STeacher Teacher = new STeacher();
-
-        //        Teacher.Load(Row, Connection.AccessPoint.Name);
-
-        //        Teachers.Add(Teacher);
-        //    }
-        //    #endregion
-
-        //    return Teachers;
-        //}
-
         /// <summary>
         /// 根據學年度學期從多個來源下載資料
         /// </summary>
@@ -198,7 +153,6 @@ namespace Sunset.Data.Integration
             #region 取得不同資料來源的教師，使用非同步執行
             SIntegrationResult<STeacher> Result = new SIntegrationResult<STeacher>();
 
-            //Parallel.ForEach(Connections.ToConcurrentQueue(), x =>
             Connections.ForEach(x =>
             {
                 try
@@ -334,86 +288,6 @@ namespace Sunset.Data.Integration
             return new Tuple<List<STeacher>, string>(Teachers, strBuilder.ToString());
         }
 
-        ///// <summary>
-        ///// 取得測試教師資料
-        ///// </summary>
-        ///// <param name="Connections"></param>
-        ///// <returns></returns>
-        //public static SIntegrationResult<STeacher> SelectTest(List<Connection> Connections)
-        //{
-        //    return Select(Connections, NativeQuery.TeacherSQL);
-        //}
-
-        ///// <summary>
-        ///// 取得所有教師資料做測試
-        ///// </summary>
-        ///// <param name="Connections"></param>
-        ///// <returns></returns>
-        //public static SIntegrationResult<STeacher> SelectAllTest(List<Connection> Connections)
-        //{
-        //    return Select(Connections, NativeQuery.TeacherSQL);
-        //}
-
-        /// <summary>
-        /// 從多個資料來源取得教師資料
-        /// </summary>
-        /// <param name="Connections"></param>
-        /// <returns></returns>
-        public static SIntegrationResult<STeacher> Select(List<Connection> Connections,Dictionary<string, SchoolYearSemester> DSNSSchoolYearSemesters)
-        {
-            #region 取得不同資料來源的課程，使用非同步執行
-            SIntegrationResult<STeacher> Result = new SIntegrationResult<STeacher>();
-
-            //Parallel.ForEach(Connections.ToConcurrentQueue(), x =>
-            Connections.ForEach(x =>
-            {
-                try
-                {
-                    if (DSNSSchoolYearSemesters.ContainsKey(x.AccessPoint.Name))
-                    {
-                        //從這裡改為新的授課教師
-                        List<STeacher> Teachers = SelectByCourseSection(x,
-                            DSNSSchoolYearSemesters[x.AccessPoint.Name].SchoolYear,
-                            DSNSSchoolYearSemesters[x.AccessPoint.Name].Semester);
-                        Result.Data.AddRange(Teachers);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Result.AddMessage("下載教師資料時發生錯誤，連線來源『" + x.AccessPoint.Name + "』");
-                    Result.AddMessage(e.Message);
-                    Result.IsSuccess = false;
-                }
-            }
-            );
-            #endregion
-
-            #region 若有超過一個以上的連線資訊，則將同名同姓的教師合併
-            if (Result.IsSuccess)
-            {
-                if (Connections.Count > 1)
-                {
-                    try
-                    {
-                        Tuple<List<STeacher>, string> MergeResult = MergeByName(Result.Data);
-                        Result.Data = MergeResult.Item1;
-                        Result.AddMessage(MergeResult.Item2);
-                    }
-                    catch (Exception e)
-                    {
-                        Result.AddMessage("合併教師資源時發生錯誤");
-                        Result.AddMessage(e.Message);
-                        Result.IsSuccess = false;
-                    }
-                }
-
-                Result.AddMessage("已成功下載" + Result.Data.Count + "筆教師資料!");
-            }
-            #endregion
-
-            return Result;
-        }
-
         /// <summary>
         /// 從多個資料來源取得教師資料
         /// </summary>
@@ -424,7 +298,6 @@ namespace Sunset.Data.Integration
             #region 取得不同資料來源的課程，使用非同步執行
             SIntegrationResult<STeacher> Result = new SIntegrationResult<STeacher>();
 
-            //Parallel.ForEach(Connections.ToConcurrentQueue(), x =>
             Connections.ForEach(x =>
             {
                 try
@@ -467,68 +340,6 @@ namespace Sunset.Data.Integration
 
             return Result;
         }
-      
-        ///// <summary>
-        ///// 從多個資料來源取得教師資料
-        ///// </summary>
-        ///// <param name="Connections">多個連線資訊</param>
-        ///// <param name="SchoolYear">學年度</param>
-        ///// <param name="Semester">學期</param>
-        ///// <returns></returns>
-        //public static SIntegrationResult<STeacher> Select(List<Connection> Connections,string SQL)
-        //{
-        //    #region 取得不同資料來源的課程，使用非同步執行
-        //    SIntegrationResult<STeacher> Result = new SIntegrationResult<STeacher>();
-
-        //    //Parallel.ForEach(Connections.ToConcurrentQueue(), x =>
-        //    Connections.ForEach(x=>
-        //    {
-        //        try
-        //        {
-        //            List<STeacher> Teachers = Select(x, SQL);
-        //            Result.Data.AddRange(Teachers);
-        //        }catch(Exception e)
-        //        {
-        //            Result.AddMessage("下載教師資料時發生錯誤，連線來源『" + x.AccessPoint.Name + "』");
-        //            Result.AddMessage(e.Message);
-        //            Result.IsSuccess = false;
-        //        }
-        //    }
-        //    );
-        //    #endregion
-
-        //    #region 若有超過一個以上的連線資訊，則將同名同姓的教師合併
-        //    if (Result.IsSuccess)
-        //    {
-        //        if (Connections.Count > 1)
-        //        {
-        //            try
-        //            {
-        //                Tuple<List<STeacher>, string> MergeResult = MergeByName(Result.Data);
-        //                Result.Data = MergeResult.Item1;
-        //                Result.AddMessage(MergeResult.Item2);
-        //            }
-        //            catch (Exception e)
-        //            {
-        //                Result.AddMessage("合併教師資源時發生錯誤");
-        //                Result.AddMessage(e.Message);
-        //                Result.IsSuccess = false;
-        //            }
-        //        }
-
-        //        Result.AddMessage("已成功下載"+Result.Data.Count+"筆教師資料!");
-        //    }
-        //    #endregion
-
-        //    return Result;
-
-        //    #region VB
-        //    //    RaiseEvent CreateDBProgress(40)
-        //    //    CopyTable cnSQL, cnJET, _
-        //    //    "SELECT DISTINCT Teacher.* FROM Teacher INNER JOIN Course ON Teacher.TeacherID=Course.TeacherID WHERE Course.SchoolYear=" & SchoolYear & " AND Course.Semester=" & Semester & " ORDER BY Teacher.TeacherName", _
-        //    //    "Teacher"
-        //    #endregion
-        //}
         #endregion
     }
 }

80d752ca7bd1dc355a68f8a0a49e6aa8cc46bcd6
kunhsiang <kunhsiang.chang@ischool.com.tw>
2013-06-04 15:51:09 +0800

    移除using

--- ischedule/test/JSScriptManager.cs
+++ ischedule/test/JSScriptManager.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.Runtime.InteropServices;
 using System.Web.Script.Serialization;
+using System.Windows.Forms;
 using Sunset.Data;
 
 namespace ischedule
